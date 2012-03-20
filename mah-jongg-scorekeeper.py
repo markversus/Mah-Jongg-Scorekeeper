0ef84406a1dfc4b1fb48ef2b1c02edc1d18632e1
Mark Johnson <mark@nineteentwelve.co.uk>
2012-03-20 15:47:59 +0000

    Made classes out of everything (on the assumption that this is the right thing to do)

--- mah-jongg-scorekeeper.py
+++ mah-jongg-scorekeeper.py
@@ -1,6 +1,6 @@
 #-------------------------------------------------------------------------------
 # Name:        MahjonggScorer
-# Version:     0.0
+# Version:     0.1
 # Purpose:     This module is an payment calculator for Mah Jongg hands. It is
 #              designed to be a Python learning tool for me and should get
 #              incrementally more complex as I (hopefully) improve.
@@ -13,6 +13,102 @@
 #-------------------------------------------------------------------------------
 #!/usr/bin/env python
 
+# Define Game Class
+class Game():
+    def __init__(self):
+        self.start_game()
+        self.run_game()
+
+    def start_game(self):
+        print("Welcome to the MarkVersus Mah Jongg Scorekeeper")
+        self.starting_chips = int(input("Please enter the initial chip balance: "))
+        self.running = True
+        # Instantiate the four players
+        self.player1 = Player(input("Player 1 Name:"), self.starting_chips)
+        self.player2 = Player(input("Player 2 Name:"), self.starting_chips)
+        self.player3 = Player(input("Player 3 Name:"), self.starting_chips)
+        self.player4 = Player(input("Player 4 Name:"), self.starting_chips)
+        self.playerlist = [self.player1, self.player2, self.player3, self.player4]
+        
+    def run_game(self):
+        # Collecting the hand scores
+        self.sorted_pl = []
+        self.temp_list = []
+        
+        while self.running == True:
+            self.winner = input("Who won this hand {} {} {} or {}?".format(self.player1.name,
+                                                                      self.player2.name,
+                                                                      self.player3.name,
+                                                                      self.player4.name,))
+            print("Please enter the scores:")
+            for self.player in self.playerlist:
+                self.player.hand_score = int(input(self.player.name))
+                if self.player.name == self.winner:
+                    self.player.is_hand_winner = True
+                    self.winning_score = self.player.hand_score
+                    self.sorted_pl.append(self.player)
+                else:
+                    self.temp_list.append(self.player)
+                    self.temp_list.sort(key=lambda x: x.hand_score, reverse=True)
+            self.sorted_pl.extend(self.temp_list)
+            print("{} won this hand with a winning score of {}".format(self.winner,
+                                                                       self.winning_score,))
+        
+            # The transactions.
+            # Works but is ugly as hell. Needs review to tidy it up both programatically
+            # and in syntax.
+            x = (self.sorted_pl[0].hand_score - self.sorted_pl[3].hand_score)*2
+            if x < 0:
+                x = 0
+            else:
+                pass
+            self.sorted_pl[3].total_score -= x
+            self.sorted_pl[0].total_score += x
+        
+            y = (self.sorted_pl[0].hand_score - self.sorted_pl[2].hand_score)*2
+            if y < 0:
+                y = 0
+            else:
+                pass
+            self.sorted_pl[2].total_score -= y
+            self.sorted_pl[0].total_score += y
+            z = (self.sorted_pl[0].hand_score - self.sorted_pl[1].hand_score)*2
+            if z < 0:
+                z = 0
+            else:
+                pass
+            self.sorted_pl[1].total_score -= z
+            self.sorted_pl[0].total_score += z
+        
+            self.sorted_pl[3].total_score -= (self.sorted_pl[2].hand_score -
+                                                 self.sorted_pl[3].hand_score)
+            self.sorted_pl[2].total_score += (self.sorted_pl[2].hand_score -
+                                                 self.sorted_pl[3].hand_score)
+            self.sorted_pl[3].total_score -= (self.sorted_pl[1].hand_score -
+                                                 self.sorted_pl[3].hand_score)
+            self.sorted_pl[1].total_score += (self.sorted_pl[1].hand_score -
+                                                 self.sorted_pl[3].hand_score)
+            self.sorted_pl[2].total_score -= (self.sorted_pl[1].hand_score -
+                                                 self.sorted_pl[2].hand_score)
+            self.sorted_pl[1].total_score += (self.sorted_pl[1].hand_score -
+                                                 self.sorted_pl[2].hand_score)
+        
+            #Display the scores
+            print("The current scores are:")
+            for self.player in self.playerlist:
+                print(self.player.name+" : "+str(self.player.total_score))
+        
+            self.play_again = input("Play another hand? (y/n)")
+        
+            # End the game
+            if self.play_again == "n":
+                print("The final scores are:")
+                for self.player in self.playerlist:
+                    print(self.player.name+" : "+str(self.player.total_score))
+                self.running = False
+            else:
+                pass
+
 # Define Player Class
 class Player():
     def __init__(self, name, starting_chips):
@@ -21,93 +117,8 @@ class Player():
         self.hand_score = 0
         self.is_hand_winner = False
 
-# Start the game
-print("Welcome to the MarkVersus Mah Jongg Scorekeeper")
-starting_chips = int(input("Please enter the initial chip balance: "))
-running = True
-
-# Instantiate the four players
-player1 = Player(input("Player 1 Name:"), starting_chips)
-player2 = Player(input("Player 2 Name:"), starting_chips)
-player3 = Player(input("Player 3 Name:"), starting_chips)
-player4 = Player(input("Player 4 Name:"), starting_chips)
-playerlist = [player1, player2, player3, player4]
-
-# Collecting the hand scores
-sorted_pl = []
-temp_list = []
-
-while running == True:
-    winner = input("Who won this hand {} {} {} or {}?".format(player1.name,
-                                                              player2.name,
-                                                              player3.name,
-                                                              player4.name,))
-    print("Please enter the scores:")
-    for player in playerlist:
-        player.hand_score = int(input(player.name))
-        if player.name == winner:
-            player.is_hand_winner = True
-            winning_score = player.hand_score
-            sorted_pl.append(player)
-        else:
-            temp_list.append(player)
-            temp_list.sort(key=lambda x: x.hand_score, reverse=True)
-    sorted_pl.extend(temp_list)
-    print("{} won this hand with a winning score of {}".format(winner,
-                                                               winning_score,))
-
-    # The transactions.
-    # Works but is ugly as hell. Needs review to tidy it up both programatically
-    # and in syntax.
-    x = (sorted_pl[0].hand_score - sorted_pl[3].hand_score)*2
-    if x < 0:
-        x = 0
-    else:
-        pass
-    sorted_pl[3].total_score -= x
-    sorted_pl[0].total_score += x
-
-    y = (sorted_pl[0].hand_score - sorted_pl[2].hand_score)*2
-    if y < 0:
-        y = 0
-    else:
-        pass
-    sorted_pl[2].total_score -= y
-    sorted_pl[0].total_score += y
-    z = (sorted_pl[0].hand_score - sorted_pl[1].hand_score)*2
-    if z < 0:
-        z = 0
-    else:
-        pass
-    sorted_pl[1].total_score -= z
-    sorted_pl[0].total_score += z
-
-    sorted_pl[3].total_score -= (sorted_pl[2].hand_score -
-                                         sorted_pl[3].hand_score)
-    sorted_pl[2].total_score += (sorted_pl[2].hand_score -
-                                         sorted_pl[3].hand_score)
-    sorted_pl[3].total_score -= (sorted_pl[1].hand_score -
-                                         sorted_pl[3].hand_score)
-    sorted_pl[1].total_score += (sorted_pl[1].hand_score -
-                                         sorted_pl[3].hand_score)
-    sorted_pl[2].total_score -= (sorted_pl[1].hand_score -
-                                         sorted_pl[2].hand_score)
-    sorted_pl[1].total_score += (sorted_pl[1].hand_score -
-                                         sorted_pl[2].hand_score)
-
-    #Display the scores
-    print("The current scores are:")
-    for player in playerlist:
-        print(player.name+" : "+str(player.total_score))
-
-    play_again = input("Play another hand? (y/n)")
-
-    # End the game
-    if play_again == "n":
-        print("The final scores are:")
-        for player in playerlist:
-            print(player.name+" : "+str(player.total_score))
-        running = False
-    else:
-        pass
-
+def main():
+    game = Game()
+    
+if __name__ == '__main__':
+    main()
